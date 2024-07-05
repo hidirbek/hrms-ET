5f5e0a16988be175070cbbebb536f727c92085f8
Khidirbek <108168179+hidirbek@users.noreply.github.com>
2024-07-05 09:26:08 +0500

    architechture was built

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Routes from "./routes";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Layout } from "../../components";
+import Main from "../../components/main/Main";
+
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        <Main />
+      </Layout>
+    </>
+  );
+};
+
+export default Home;

7b3eb836f07c3fcbbc13e670e0c8f20788832bcd
Khidirbek <108168179+hidirbek@users.noreply.github.com>
2024-07-22 16:13:40 +0500

    login page has done

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../header/Header";
 import Footer from "../footer/Footer";
+import Sidebar from "../sidebar/Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <Sidebar />
       <main>{children}</main>
       <Footer />
     </>
